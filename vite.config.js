3d2fdb85ad327a6817c579f2020e0808b9cc6672
parvvvv <goelparv9@gmail.com>
2025-06-26 12:07:14 +0530

    INITIAL COMMIT

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/adslandingpage/', // very important
+  // very important
   plugins: [react()],
 })
