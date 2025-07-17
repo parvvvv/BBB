e9c5c70105faa69f5ca368c3cd73c2335a8cfbbb
parvvvv <goelparv9@gmail.com>
2025-07-17 16:57:06 +0530

    Changes after feedback

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -3,12 +3,14 @@ import './ThankYou.css';
 
 const ThankYou = () => {
   return (
-    <div className="thank-you-page">
-      <h1>Thank You!</h1>
-      <p>
-        We’ve received your message. Our team will get back to you shortly to discuss your app development requirements.
-      </p>
-      <a href="/" className="back-home">← Back to Home</a>
+    <div className="thank-you-wrapper">
+      <div className="thank-you-content">
+        <h1>Thank You!</h1>
+        <p>
+          We’ve received your message. Our team will get back to you shortly to discuss your app development requirements.
+        </p>
+        <a href="/" className="back-home">← Back to Home</a>
+      </div>
     </div>
   );
 };
