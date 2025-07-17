2cb79dda170aed4a49874d11782c522f6951da51
parvvvv <goelparv9@gmail.com>
2025-07-17 16:41:44 +0530

    Changes after feedback

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/adslandingpage/', // very important
+  base: '/', // very important
   plugins: [react()],
 })
