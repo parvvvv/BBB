05f7cb0919f54b98a79fc363f1cc28f8c31eba94
parvvvv <goelparv9@gmail.com>
2025-07-17 17:15:10 +0530

    Changes after feedback

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './ThankYou.css';
+import { Link } from 'react-router-dom';
 
 const ThankYou = () => {
   return (
     <div className="thank-you-wrapper">
       <div className="thank-you-content">
-        <h1>Thank You!</h1>
+        <h1>Thank You for Contacting Us</h1>
         <p>
           We’ve received your message. Our team will get back to you shortly to discuss your app development requirements.
         </p>
-        <a href="/" className="back-home">← Back to Home</a>
+        <div className="links-box">
+          <Link to="/" className="back-home">← Back to Home</Link>
+          <a href="/#contact-form" className="back-home">📞 Contact Again</a>
+        </div>
       </div>
     </div>
   );
